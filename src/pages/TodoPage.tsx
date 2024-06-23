3d3cb356586160b674cf020ed5dee070789ea05e
nissy0903 <nisihiro44@gmail.com>
2024-06-23 15:21:32 +0900

    Changed 6 23 2024 15:21

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,34 +1,53 @@
-//import React,{ useState } from "react";
-
 const TodoPage = () => {
-    //const [todoText, setTodoText] = useState("");
-    //const [todoList, setNewTodoList] = React.useState([]);
-    return(
-        <div className="max-height todo">
-            <h1>Todoリスト</h1>
-            {/*
-            <table>
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>コメント</td>
-                        <td>状態</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {todoList.map((todo, index) => (
-                        <tr>
-                            <td>{index}</td>
-                            <td>{todo.comment}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            */}
-            <p>製作中</p>
-        </div>
+    return (
+        <p className="max-height">製作中</p>
     )
 }
+
+/*
+import React,{ useState } from "react";
+
+const TodoPage = () => {
+    const [todoText, setTodoText] = useState("");
+    const [todoList, setNewTodoList] = React.useState([]);
+
+    const onChangeTodoText = (e : ChangeEvent<HTMLInputElement>) => {
+        setTodoText(e.target.value);
+    }
+    return(
+        <>
+            <div className="task-area">
+                <h1>ToDoリスト</h1>
+                <table>
+                <thead>
+                    <tr>
+                    <td >ID</td>
+                    <td>コメント</td>
+                    <td>状態</td>
+                    </tr>
+                </thead>
+                <tbody id="todo-body">  
+                    {todoList.map((todo, index) => (
+                    <tr>
+                        <td>{index}</td>
+                        <td>{todo.comment}</td>
+                        <td><button>{todo.status}</button></td>
+                        <td><button>削除</button></td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+
+            <h2>新規タスクの追加</h2>
+            <div className="add-todo">
+                <input value={todoText} onChange={onChangeTodoText} />
+                <button onClick={onClickAdd}>追加</button>
+            </div>
+        </>
+    )
+}
+*/
 export default TodoPage;
 
 /*
